22b6158ebc42956034c37b10d7cc0b56c386132e
moonkorea00 <moonkorea00@gmail.com>
2023-09-24 03:54:36 +0900

    fix : API authentication issue add passportKey param

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,13 +9,15 @@ export async function fetchCorrections(
 ) {
   const chunk = splitChunks(editor, text);
 
-  const url = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy';
+  const URL = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy';
+  const PASSPORT_KEY = '633b693a365e691612b9a81486ebc48a4c630a60';
 
   const promises = chunk.map(value =>
-    axios.get(url, {
+    axios.get(URL, {
       params: {
         color_blindness: 0,
         q: value,
+        passportKey: PASSPORT_KEY,
       },
     })
   );
@@ -28,7 +30,6 @@ export async function fetchCorrections(
     },
     async () => {
       const res = await Promise.all(promises);
-
       const data = res.reduce(
         (acc, val) => {
           return {
