bdad4e2371b284294ea5629dcc5b243b9b872613
moonkorea00 <moonkorea00@gmail.com>
2023-09-27 17:03:26 +0900

    fix : renew key on authentication error

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,26 +1,82 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import * as vscode from 'vscode';
 import axios from 'axios';
-import { splitChunks } from './api.utils';
+import { splitTextToChunks } from './api.utils';
+import { workspace } from '../command/command.utils';
 
 export async function fetchCorrections(
+  context: vscode.ExtensionContext,
   editor: vscode.TextEditor,
   text: string
 ) {
-  const chunk = splitChunks(editor, text);
+  let passportKey = workspace.get<string>(context, 'passport_key');
 
-  const URL = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy';
-  const PASSPORT_KEY = '633b693a365e691612b9a81486ebc48a4c630a60';
+  if (!passportKey) {
+    passportKey = await fetchPassportKey();
+    if (passportKey) {
+      workspace.set<string>(context, 'passport_key', passportKey);
+    }
+  }
 
-  const promises = chunk.map(value =>
-    axios.get(URL, {
-      params: {
-        color_blindness: 0,
-        q: value,
-        passportKey: PASSPORT_KEY,
-      },
-    })
-  );
+  async function attemptCorrection() {
+    const BASE_URL =
+      'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy';
+    const MAX_RETRIES = 2;
+    let retries = 0;
+
+    const chunks = splitTextToChunks(editor, text);
+
+    while (retries < MAX_RETRIES) {
+      try {
+        const promises = chunks.map(value =>
+          axios.get(BASE_URL, {
+            params: {
+              color_blindness: 0,
+              q: value,
+              passportKey,
+            },
+          })
+        );
+        const res = await Promise.all(promises);
+
+        if (res[0].data.message.error === '유효한 키가 아닙니다.') {
+          throw new Error('invalid key');
+        }
+
+        const combinedCorrection = res.reduce(
+          (acc, val) => {
+            return {
+              ...acc,
+              notag: acc.notag + val.data.message.result.notag_html,
+              html: acc.html + val.data.message.result.html,
+            };
+          },
+          { notag: '', html: '' }
+        );
+
+        return combinedCorrection;
+      } catch (err) {
+        retries++;
+        if (err instanceof Error) {
+          if (err.message === 'invalid key') {
+            passportKey = await fetchPassportKey();
+            if (passportKey) {
+              workspace.set<string>(context, 'passport_key', passportKey);
+            }
+          }
+          if (err.message === 'Failed to get key') {
+            throw new Error(err.message);
+          }
+        } else {
+          throw err;
+        }
+      }
+    }
+
+    if (retries === MAX_RETRIES) {
+      throw new Error('Max retry exceeded.');
+    }
+  }
 
   const correction = vscode.window.withProgress(
     {
@@ -28,22 +84,22 @@ export async function fetchCorrections(
       title: 'Running grammar check..',
       cancellable: true,
     },
-    async () => {
-      const res = await Promise.all(promises);
-      const data = res.reduce(
-        (acc, val) => {
-          return {
-            ...acc,
-            notag: acc.notag + val.data.message.result.notag_html,
-            html: acc.html + val.data.message.result.html,
-          };
-        },
-        { notag: '', html: '' }
-      );
-
-      return data;
-    }
-  );
+    attemptCorrection
+  ) as Thenable<{ notag: string; html: string }>;
 
   return correction;
+}
+
+async function fetchPassportKey() {
+  const BASE_URL =
+    'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=맞춤법검사기';
+
+  try {
+    const { data: html } = await axios.get(BASE_URL);
+    const match = /passportKey=([a-zA-Z0-9]+)/.exec(html);
+
+    if (match) return match[1];
+  } catch (err) {
+    throw new Error('Failed to get key');
+  }
 }
--- src/command/check/index.ts
+++ src/command/check/index.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import type { AxiosError } from 'axios';
 import * as vscode from 'vscode';
-import { AxiosError, isAxiosError } from 'axios';
-import { renderPanel } from '../../panel';
+import { isAxiosError } from 'axios';
 import { fetchCorrections } from '../../api';
+import { renderPanel } from '../../panel';
 import {
+  workspace,
   getChunkIndicies,
   showVscodeMessage,
-  updateCorrectionState,
 } from '../command.utils';
 
 export async function checkGrammar(context: vscode.ExtensionContext) {
@@ -14,53 +15,75 @@ export async function checkGrammar(context: vscode.ExtensionContext) {
 
   if (!editor) return;
 
-  const fullEditorText = editor.document.getText();
-  const selectedEditorText = editor.document.getText(editor.selection);
-  const text = selectedEditorText||fullEditorText;
+  const documentText = editor.document.getText();
+  const selectedText = editor.document.getText(editor.selection);
+  const editorText = selectedText || documentText;
+
+  if (!editorText) return;
 
   try {
-    if (!selectedEditorText && !fullEditorText) return;
+    const { html, notag } = await fetchCorrections(context, editor, editorText);
 
-    const data = await fetchCorrections(editor, text);
-
-    // render panel
-    renderPanel(data, context);
-
-    // store state
-    if (selectedEditorText) {
-      const position = getChunkIndicies(editor);
-      updateCorrectionState(
-        context,
-        null,
-        null,
-        selectedEditorText,
-        data.notag,
-        position
-      );
-    } else {
-      updateCorrectionState(context, text, data.notag);
-    }
+    renderAndStoreCorrections(
+      html,
+      notag,
+      context,
+      editor,
+      selectedText,
+      documentText
+    );
   } catch (err) {
-    const errorMap = {
-      ENOTFOUND: `Unable to connect to the server. Please check your network connection.`,
-      range: (status: number) =>
-        status >= 400 && status < 500
-          ? `Client Error: ${(err as AxiosError).response?.status}`
-          : `Potential server error. Immediate Request after another can lead to an error.`,
-      default: `An unexpected error occurred. Please try again later.`,
-    };
-
     if (isAxiosError(err)) {
-      const errorMessage =
-        errorMap.range(Number(err.response?.status)) ||
-        errorMap[err?.code as keyof typeof errorMap] ||
-        errorMap.default;
-      return showVscodeMessage('axiosError', {
-        errorMessage: errorMessage!,
-      });
+      return handleAxiosError(err);
+    } else if (err instanceof Error) {
+      return showVscodeMessage('unexpected', err.message);
     } else {
-      console.log(err);
-      return showVscodeMessage('unexpected');
+      showVscodeMessage('unexpected');
     }
   }
+}
+
+function renderAndStoreCorrections(
+  html: string,
+  notag: string,
+  context: vscode.ExtensionContext,
+  editor: vscode.TextEditor,
+  selectedText: string,
+  documentText: string
+) {
+  renderPanel(html, context);
+
+  if (selectedText) {
+    const positions = getChunkIndicies(editor);
+
+    workspace.set(context, 'correction', {
+      editorText: selectedText,
+      correction: notag,
+      chunkPosition: positions,
+    });
+  } else {
+    workspace.set(context, 'correction', {
+      editorText: documentText,
+      correction: notag,
+      chunkPosition: null,
+    });
+  }
+}
+
+function handleAxiosError(err: AxiosError) {
+  const errorMap = {
+    ENOTFOUND: `Unable to connect to the server. Please check your network connection.`,
+    range: (status: number) =>
+      status >= 400 && status < 500
+        ? `Client Error: ${err.response?.status}`
+        : `Potential server error. Immediate Request after another can lead to an error.`,
+    default: `An unexpected error occurred. Please try again later.`,
+  };
+
+  const errorMessage =
+    errorMap.range(Number(err.response?.status)) ||
+    errorMap[err?.code as keyof typeof errorMap] ||
+    errorMap.default;
+
+  return showVscodeMessage('axiosError', errorMessage);
 }
